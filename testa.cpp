a36cf8739f5685cb3c9cacfe7f806a15da363152
Santanu Bandyopadhyay <santanub369@gmail.com>
2026-06-06 12:32:12 +0530

    No of subarray with sum k

--- testa.cpp
+++ testa.cpp
@@ -3,42 +3,36 @@
 using namespace std;
 using ll = long long;
 
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        int n = nums.size();
-        if (n==0) return 0;
-        int max_count = 1;
-        int count = 1;
-        set<int> s;
-        for (int i = 0; i < n; i++)
-        {
-            s.insert(nums[i]);
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            if (s.find(nums[i]-1) == s.end()) {
-                count = 1;
-                int x = nums[i];
-                while (s.find(x+1) != s.end())
-                {
-                    count ++;
-                    x++;
-                }
-                max_count = max(max_count, count);
-            }
-        }
-        return max_count;
-        
-        
-    }
-};
+// Prefix sum
+// if a sum exist in the past where its sum-k then we can exclue that part and form a sum array
+// there can be multiple sub arrays with same last index so wen need map
+// when sum == k then we need 0 in the past to we have to add that manually at first
 
                                                      
 int main() {
-    vector <int> v  =  {100,4,200,1,3,2};
-    Solution sol;
-    cout << sol.longestConsecutive(v);
+    int n = 10;
+    int arr[] = {1,2,3,-3,1,1,1,4,2,-3};
+    int k = 3;
+    int sum = 0;
+
+
+    int sub_arr_cnt = 0;
+    set <int> s;
+    map<int, int> mp;
+    mp.insert({0,1});
+    for (int i = 0; i < n; i++)
+    {
+        sum +=  arr[i];
+
+        sub_arr_cnt+= mp[sum-k];
+
+        mp[sum]++;
+    }
+
+    cout << sub_arr_cnt;
+    
+
+    
+
     return 0;
 }
